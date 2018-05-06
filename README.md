a5af008d8bffc73b97b4314ef57409e4a1bde359
KOZ39 <2yoonj@naver.com>
2018-05-06 10:41:33 +0900

    임시 레벨 변수 제거 & 경험치 수식 변경

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # gfOperationReportCalculator
 
 소녀전선 작전보고서 계산기
+
+## 도움을 주신 분들
+* marty
+* 에무1918
--- js/calculator.js
+++ js/calculator.js
@@ -134,32 +134,29 @@ function reportCalc() {
 	var currentLv = Number(document.getElementById("currentLv").value);
 	var currentExp = Number(document.getElementById("currentExp").value);
 	var targetLv = Number(document.getElementById("targetLv").value);
-	var tempLv = 0;
 	var report = 0;
 
 	if(fairy == 1 && targetLv < dollAccExp.length || fairy == 3 && targetLv <= 100) {
 		if((dollAccExp[currentLv+1] - dollAccExp[currentLv]) * fairy > currentExp && currentExp >= 0 && targetLv > currentLv) {
 			if(targetLv > 115) {
-				tempLv = Math.max(currentLv, 115);
-				report += Math.ceil((dollAccExp[targetLv] - dollAccExp[tempLv]) / (3000 * oath));
+				report += Math.ceil((dollAccExp[targetLv] - dollAccExp[Math.max(currentLv, 115)] - currentExp) / (3000 * oath));
 				targetLv = 115;
+				currentExp = 0;
 			}
 			if(targetLv > 110 && currentLv < 115) {
-				tempLv = Math.max(currentLv, 110);
-				report += Math.ceil((dollAccExp[targetLv] - dollAccExp[tempLv]) / (3000 * oath));
+				report += Math.ceil((dollAccExp[targetLv] - dollAccExp[Math.max(currentLv, 110)] - currentExp) / (3000 * oath));
 				targetLv = 110;
+				currentExp = 0;
 			}
 			if(targetLv > 100 && currentLv < 110) {
-				tempLv = Math.max(currentLv, 100);
-				report += Math.ceil((dollAccExp[targetLv] - dollAccExp[tempLv]) / (3000 * oath));
+				report += Math.ceil((dollAccExp[targetLv] - dollAccExp[Math.max(currentLv, 100)] - currentExp) / (3000 * oath));
 				targetLv = 100;
+				currentExp = 0;
 			}
 
 			if(targetLv <= 100 && currentLv < 100) {
-				report += Math.ceil((dollAccExp[targetLv] - dollAccExp[currentLv]) / 3000);
+				report += Math.ceil((dollAccExp[targetLv] - dollAccExp[currentLv] - currentExp) / 3000);
 			}
-
-			report -= Math.floor(currentExp / 3000)
 
 			document.getElementById("reportCalcResult").innerText = report * fairy;
 		}
